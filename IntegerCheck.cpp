c2f572a4670a3e1e40ee930acb22e73581c43cf8
bsc-com-42-24 <bsc-com-42-24@unima.ac.mw>
2026-03-30 22:36:53 +0200

    Update IntegerCheck.cpp with new functionality

--- IntegerCheck.cpp
+++ IntegerCheck.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
-    
+    int value;
+    bool valid = false;
+
+    while (!valid) {
+        cout << "Enter an integer value between 5 and 10: " << endl;
+
+        if (cin >> value) { 
+            if (value >= 5 && value <= 10) {
+                valid = true; 
+            } else {
+                cout << "You entered " << value 
+                     << ". Please enter a9 number between 5 and 10." 
+                     << endl;
+            }
+        } else {
+            cout << "Sorry, you entered an invalid number, please try again." << endl;
+            cin.clear(); 
+            cin.ignore(1000, '\n'); 
+        } 
+    }
+
+    cout << "Your input value (" << value << ") has been accepted." << endl;
+
+    return 0;
 }
